2688f337d85bc8ef2e55ff4ca35d37a19611da47
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-07-08 21:36:06 +0200

    Add message counter to home page & login icon if logged out

--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -1,11 +1,48 @@
-import styles from './Homepage.module.css'
+import styles from './Homepage.module.css';
+import { useEffect, useState } from 'react';
+import { useOutletContext } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import Icon from '@mdi/react';
+import {
+  mdiLogin,
+} from "@mdi/js";
 
 function Homepage() {
+  const [messagesCount, setMessagesCount] = useState([]);
+  const [token, , user] = useOutletContext();
+  useEffect(() => {
+    if (user._id) {
+      const postApi = async () => {
+        try {
+          const res = await fetch(
+            `http://localhost:3000/${user._id}/messages-num`,
+            {
+              headers: {
+                Authorization: token,
+              },
+            }
+          );
+          const data = await res.json();
+
+          setMessagesCount(data);
+        } catch (err) {
+          console.log(err.name);
+        }
+      };
+      postApi();
+    }
+    return () => {
+      setMessagesCount([]);
+    };
+  }, [user, token]);
+  console.log(messagesCount);
   return (
     <div className={styles.home}>
-    <h2>Welcome to messagin App</h2>
-    <h3>You have 5 messages</h3>
+      <h2>Welcome to messaging App</h2>
+      {token ? <h3>You have {messagesCount} {messagesCount <= 1 ? 'message': 'messages'}</h3> : <Link className={styles.login} to='/login'><Icon path={mdiLogin} size={5}></Icon></Link>}
+      
+      
     </div>
-  )
+  );
 }
 export default Homepage;
