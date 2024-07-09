e0c90a0532bc394ddc771a29a2ad86aed8cfd9e0
WalterBigZ <150455054+BigWojtek7@users.noreply.github.com>
2024-07-09 12:13:20 +0200

    Change display from grid to flex for messages cards

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -9,43 +9,49 @@ import {
   mdiHome,
   mdiAccount,
   mdiMessage,
-  mdiMessagePlus
-} from "@mdi/js";
-
+  mdiMessagePlus,
+} from '@mdi/js';
 
 function Sidebar({ token }) {
   return (
     <div className={styles.sidebar}>
-      <div className={styles.menu}><Icon path={mdiViewDashboard} size={2}/><h1>Menu:</h1></div>
+      <div className={styles.menu}>
+        <Icon path={mdiViewDashboard} size={2} />
+        <h1>Menu:</h1>
+      </div>
       <ul>
         <li>
-        <Icon path={mdiHome} size={1.4}/>
+          <Icon path={mdiHome} size={1.4} />
           <Link to="/">Home</Link>
         </li>
         {!token && (
           <>
             <li>
-            <Icon path={mdiAccountPlus} size={1.4}/>
+              <Icon path={mdiAccountPlus} size={1.4} />
               <Link to="sign-up">Sign Up</Link>
             </li>
             <li>
-            <Icon path={mdiLogin} size={1.4}/>
+              <Icon path={mdiLogin} size={1.4} />
               <Link to="login">Log-in</Link>
             </li>
           </>
         )}
-        <li>
-        <Icon path={mdiAccount} size={1.4}/>
-          <Link to="profile">Profile</Link>
-        </li>
-        <li>
-        <Icon path={mdiMessage} size={1.4}/>
-          <Link to="messages">Messages</Link>
-        </li>
-        <li>
-        <Icon path={mdiMessagePlus} size={1.4}/>
-          <Link to="create-message">Create Message</Link>
-        </li>
+        {token && (
+          <>
+            <li>
+              <Icon path={mdiAccount} size={1.4} />
+              <Link to="profile">Profile</Link>
+            </li>
+            <li>
+              <Icon path={mdiMessage} size={1.4} />
+              <Link to="messages">Messages</Link>
+            </li>
+            <li>
+              <Icon path={mdiMessagePlus} size={1.4} />
+              <Link to="create-message">Create Message</Link>
+            </li>
+          </>
+        )}
       </ul>
     </div>
   );
